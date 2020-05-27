a9f6d1f32abad159f003c21900ea0dce9bdef811
smonticeuex <58290699+smonticeuex@users.noreply.github.com>
2020-05-27 13:47:21 -0400

    First attempt (Failed)

---
+++ script/main.js
@@ -0,0 +1,14 @@
+//Steps to take: 
+//1. functions for earth and mars time zone
+//2. add variables to functions 
+//3. make the clock visible 
+
+
+//Get the current time per user
+function earthClock {
+	var eClock = newDate(); 
+	
+	var hours = eClock.getHours();
+	var minutes = eClock.getMinutes(); 
+	var seconds = eClock.getSeconds();
+}
