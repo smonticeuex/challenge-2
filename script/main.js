726e26fdc737bab3602537d4b74fe8cf27e22e90
smonticeuex <58290699+smonticeuex@users.noreply.github.com>
2020-05-30 07:24:49 -0400

    Almost done!
    To do: link pages together and figure out code for time on Mars

--- script/main.js
+++ script/main.js
@@ -4,11 +4,20 @@
 //3. make the clock visible 
 
 
-//Get the current time per user
-function earthClock {
-	var eClock = newDate(); 
+//Get the current time per user + AM PM format
+function earthClock () {;
+	var eClock = new Date(); 
 	
 	var hours = eClock.getHours();
 	var minutes = eClock.getMinutes(); 
 	var seconds = eClock.getSeconds();
+
+
+    var amPM = (hours < 12) ? "AM" : "PM";
+
+//Display the current time 
+    document.getElementById('time').innerHTML = 
+    hours + " : "+ minutes + " : " +seconds + "" + amPM;
+	var t = setTimeout(earthClock, 500); 
+
 }
